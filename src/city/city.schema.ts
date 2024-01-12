eb8b07cbb36cd5a3c2dec37252b988375efbe4dd
hrushikesh1kale <hrushikesh1.kale@ril.com>
2024-01-12 17:04:20 +0530

    clean

--- src/city/city.schema.ts
+++ src/city/city.schema.ts
@@ -1,7 +1,4 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { HydratedDocument } from "mongoose";
-
-export type CityDocument = HydratedDocument<City>;
 
 @Schema()
 export class City {
